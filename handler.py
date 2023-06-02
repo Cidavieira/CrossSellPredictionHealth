4f9385786106f9e9009fc0077a6e2873f69beab5
Cida Vieira <95623644+Cidavieira@users.noreply.github.com>
2023-06-02 19:17:30 -0300

    first

---
+++ handler.py
@@ -0,0 +1,50 @@
+import os
+import pickle
+import pandas as pd
+from flask import Flask, request, Response
+from healthinsurance.HealthInsurance import HealthInsurance
+
+# loading model
+#model = pickle.load( open( '\\Users\\Cida\\Classificacao_PA4\\src\\models\\model_linear_regression.pkl', 'rb' ) )
+model = pickle.load( open( '\\models\\model_linear_regression.pkl', 'rb' )
+# initialize API
+app = Flask( __name__ )
+
+@app.route('/healthinsurance/predict', methods=['POST'])
+
+def healthinsurance_predict():
+    test_json = request.get_json()
+
+    if test_json: # there is data
+        if isinstance( test_json, dict ): # unique example
+            test_raw = pd.DataFrame( test_json, index=[0] )
+            
+        else: # multiple example
+            test_raw = pd.DataFrame( test_json, columns=test_json[0].keys() )
+            
+        # Instantiate HealthInsurance class
+        pipeline = HealthInsurance()
+        
+        # data cleaning
+        df1 = pipeline.data_cleaning( test_raw )
+        
+        # feature engineering
+        df2 = pipeline.feature_engineering( df1 )
+        
+        # data preparation
+        df3 = pipeline.data_preparation( df2 )
+        
+        # prediction
+        df_response = pipeline.get_prediction( model, test_raw, df3 )
+        
+        return df_response
+    
+    else:
+        return Response( '{}', status=200, mimetype='application/json' )
+    
+if __name__ == '__main__':
+    #app.run( '0.0.0.0', debug=True )
+    #port=os.environ.get('PORT',5000)
+    port = os.environ.get('PORT', 5000)
+    app.run( '0.0.0.0', port=port )
+
